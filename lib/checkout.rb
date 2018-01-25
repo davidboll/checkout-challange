f45498d9dfc3d6e1a7e47ecc4e50689b94c599ca
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-01-25 14:57:28 +0100

    added products to basket

---
+++ lib/checkout.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class Checkout
+attr_accessor :product, :basket, :total_price
+
+PRODUCT_001 = {name: 'lavender', price: 9.25}
+PRODUCT_002 = {name: 'Personalised Cufflinks', price: 45.00}
+PRODUCT_003 = {name: 'Kids T-shirt', price: 19.95}
+  def initialize
+    @basket = []
+  end
+
+  def scan(product)
+    @basket << product
+  end
+
+  def total_price
+  end
+
+end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,1 +1,28 @@
 require './lib/checkout'
+require 'pry'
+describe Checkout do
+
+
+  subject { described_class.new }
+
+  it 'adds product to basket on scan' do
+    #binding.pry
+    product = Checkout::PRODUCT_001
+    subject.scan(product)
+    #binding.pry
+    expect(subject.basket).to include product
+  end
+
+  it 'calculates total_price' do
+    product = Checkout::PRODUCT_001
+    expect(subject.total_price).to eq product.price
+  end
+
+ xit 'is should take promotional rules' do
+   expected_output
+   expect(subject.promotional_rules).to
+ end
+
+
+
+end
