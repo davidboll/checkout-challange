dbcb91a93abd7fd3a590bfb9f6809bcd9a74711a
bollen <david.boll@me.com>
2018-01-25 15:00:16 +0100

    ignore these changes

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,1 +1,10 @@
 require './lib/checkout'
+
+describe Checkout do
+
+  #subject { described_class.new() }
+
+  it 'total price should return to 66.78' do
+    expect(subject.total).to eq 66.78
+  end
+end
